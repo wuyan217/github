ed40d5d5edbb28e6f2c936573fa64415ffc1d068
wuyan217 <jianglian217@163.com>
2022-02-23 18:56:19 +0800

    第一次从idea提交代码

---
+++ heima/javaSE_code/idea_test/src/com/itheima/OperatorDemo.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+public class OperatorDemo {
+    public static void main(String[] args) {
+//        ctrl+shift+/  多行注释
+/*        System.out.println("helloworld");
+        System.out.println("黑马程序员");
+        System.out.println("java");*/
+//        ctrl+alt+l 格式化
+        int i = 10;
+        System.out.println("i：" + i);
+    }
+}
---
+++ heima/javaSE_code/myArray/src/com/itheima/ArrayDemo.java
@@ -0,0 +1,21 @@
+package com.itheima;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+
+        /*
+        * 左边  int:说明数组中的元素类型是int类型
+        *      []:说明是一个数组
+        *       arr:这是数组名称
+        * 右边   new ：为数组申请内存空间
+        *       int： 说明数组中的元素类型是int类型
+        *       []:说明是一个数组
+        *        3：数组长度，其实就是数组中的元素个数
+        *
+        *
+        * */
+//        输出数组名
+        System.out.println(arr);
+    }
+}
